cfef3bf670773dcf784cfebf845686cbc8a80059
unknown <stnm.bb2@gamil.com>
2026-07-20 22:07:58 +0300

    WIP3

--- boaz_porat/ex3_spectrogram.m
+++ boaz_porat/ex3_spectrogram.m
@@ -1,19 +1,27 @@
-spec = create_spectrogram(signal, Fs, dt, RBW, overlap, rect);
+spec = create_spectrogram(signal, Fs, dt, RBW, overlap, symetryc_rect);
 
 function spec = create_spectrogram(signal, Fs, dt, RBW, overlap, window)
-% dt - requierd time resolution
-% RBW - requierd frequncy resolution
-% overlap - precentage of overlap between two windows
-% window - anonimic function that returns the window
-buf_sig= buffer(signal, RBW, overlap);
-win = window(length(signal), )
+% dt - required time resolution
+% RBW - required frequency resolution
+% overlap - percentage of overlap between two windows
+% window - anonymous function that returns the window
+
 f_start = 1e6;
 f_stop = 2e6;
 t_start = 0;
 t_stop = 1e3;
 Fc = 1.5e6;
 
-%check rbw and dt corespond to eachother
+%check RBW and dt correspond to each other
+
+
+buf_sig= buffer(signal, RBW, overlap);
+
+delta_f = Fs/RBW;
+k = ceil(log2(delta_f));
+N = 2 ^ k;
+win = window(length(signal), N);
+
 Ts = 1 / Fs;
 t = t_start:1 / dt:t_stop - 1 / dt;
 f = f_start:1 / RBW:f_stop - 1 / RBW;
