94c6c78859a2105509957cf07f3b62ab485cedc2
TalAviaBiton <talaviabiton@gmail.com>
2026-07-20 14:50:21 +0300

    ex3 bp

---
+++ boaz_porat/ex3_spectrogram.m
@@ -0,0 +1,20 @@
+function create_spectrogram(signal, Fs, dt, RBW, overlap, window)
+% dt - requierd time resolution
+% RBW - requierd frequncy resolution
+% overlap - precentage of overlap between two windows
+% window - anonimic function that returns the window
+
+f_start = ;
+f_stop = ;
+t_start = ;
+t_stop = ;
+Fc = ;
+plt_config = [f_start, f_stop, t_start, t_stop, Fc];
+
+%check rbw and dt corespond to eachother
+
+%buffer
+%imagesc
+%set(gca, 'ydir', 'normal')
+
+end
